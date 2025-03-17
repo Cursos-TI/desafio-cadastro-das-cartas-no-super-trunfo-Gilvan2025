cf3959d4150861b103e9aa84dbe8d4e8b5b8185b
Gilvan2025 <gilvanci@bol.com.br>
2025-03-17 00:08:09 +0000

    commint

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
 int main (){  
     
-    int
-printf("Qual seu estado?\n");
-scanf("%c")
+    int população, pontos turisticos;
+    char estado, codigo da carta[04], nome da cidade[50];
+    float area em km, pib;
+
+printf("Qual a população do seu estado:?\n");
+scanf("%d",&população);
+
+printf("qual seu estado:?\n");
+scanf("%s",&estado);
+
+printf("qual a area do seu estado em km:?\n");
+scanf("%f",&area);
+
+printf("quantos pontos turisticos:?\n");
+scanf("%d",&pontos turisticos);
+
+printf("qual o pib da sua cidade:?\n");
+scanf("%f",&pib);
+
+printf("digite uma carta:\n");
+scanf("%s",%carta);
+
+printf("digite sua cidade:\n");
+scanf("%s",&cidade);
+
+printf("estado: %s - area:%f - pontos turisticos: %d", estado,area, pontos turisticos);
+printf("pib: %f - carta: %s - cidade%s", pib, carta, cidade);
+
+
+
+
+
+
+
 
 
 
